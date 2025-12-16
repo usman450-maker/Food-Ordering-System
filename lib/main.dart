377bdaacd3d6d5c34a3a1fd31ecae4904e6b0f4b
usman450-maker <muhammadusman950450@gmail.com>
2025-12-17 02:35:24 +0500

    Remove hardcoded Firebase keys

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,32 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 import 'package:resturant_app/core/router/app_router.dart';
 import 'package:resturant_app/firebase_options.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+  // 🔐 Load .env file
+  await dotenv.load();
+
+  // 🔥 Initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // 📱 Lock orientation
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
 
   @override
   Widget build(BuildContext context) {
@@ -25,4 +38,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
